1cd15c006b3b97f4fc0ba01501235f900243bc30
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-05 16:39:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
   attr_reader :name, :artist, :genre
   
   @@count = 0 
+  @@artist = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artist << artist
   end
   
 end
